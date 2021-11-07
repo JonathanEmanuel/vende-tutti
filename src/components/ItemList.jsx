acdf5541dc100472e8a7f97a6eb809fa17ba7180
Jonathan Cruz <cruzjonathan.e@gmail.com>
2021-11-06 22:08:54 -0300

    cambios minimos.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,11 +4,13 @@ import Item from './Item';
 const ItemList = ( {productos}) => {
     
     return(
-        <div className="row">
-            {productos.map( (producto) => {
-               return <Item key={producto.id} name={producto.name} price={producto.price.toLocaleString()} img={producto.img} id={producto.id} />
-                    
-            })}
+        <div className="container">
+            <div className="row">
+                {productos.map( (producto) => {
+                return <Item key={producto.id} name={producto.name} price={producto.price.toLocaleString()} img={producto.img} id={producto.id} />
+                        
+                })}
+            </div>
         </div>
     )
 }
