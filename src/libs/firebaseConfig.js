be7c2290cc91c1383bf5f46b1c95693a59ccc1f5
Jonathan Cruz <cruzjonathan.e@gmail.com>
2021-11-21 13:41:10 -0300

    +firebaseConfig

--- src/libs/firebaseConfig.js
+++ src/libs/firebaseConfig.js
@@ -1,6 +1,5 @@
-import { initializeApp  } from './firebaseConfig';
-import { getFirestore } from './firebaseConfig';
-
+import { initializeApp  } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export { app, db };
